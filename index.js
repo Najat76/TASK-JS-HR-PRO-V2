d1aa08c24d508e10aceea99519533e4d9c382120
Najat76 <safari76@hotmail.co.uk>
2023-05-18 17:40:52 +0300

    wish good solution

--- index.js
+++ index.js
@@ -1,4 +1,40 @@
-const employeesJSON = require('./employees.json');
-const managersJSON = require('./managers.json');
+const employeesJSON = require("./employees.json");
+const managersJSON = require("./managers.json");
 
-// Code Here
+class Employee {
+  //Class Properties
+  constructor(name, position, yearJoined, salary) {
+    this.name = "Najat";
+    this.position = "Team Leader";
+    this.yearJoined = "2023";
+    this.salary = "1000 KD";
+  }
+
+  //Class Methods
+  idBadge(name1) {
+    return this.name.forEach(
+      (name1) => ("Position: ", this.position, "First Name", this.name)
+    );
+  }
+}
+console.log(idBadge(name1));
+/****************************************************************** */
+/*    Inheritance                                                   */
+/****************************************************************** */
+class Manager extends Employee {
+  constructor(name, position, yearJoined, salary, bonusPercentage) {
+    super((name, position, yearJoined, salary));
+  }
+
+  function percentage (percentage) {
+    return this.percentage * this.salary;
+  }
+  salaryIncrease(increaseAmount) {
+    // their is a formula for percentage
+    /* P% * X = Y */
+
+    return this.manager.forEach((salary) => Employee.salary + salaryIncrease);
+  }
+}
+
+const bonusPercentage = new Manager(90);
